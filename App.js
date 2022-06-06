f41f261fecf82c03caf93acd566bee3e81b9ba1e
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2022-06-06 20:12:18 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { StyleSheet,View, Text,StatusBar} from 'react-native';
+import { store } from './store';
 
 export default function App() {
   return (
-    <Provider>
+    <Provider store={store}>
     <View>
       <StatusBar style="auto" />
       <Text>Open up App.js to start working on your app!</Text>
