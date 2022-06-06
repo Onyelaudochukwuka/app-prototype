29f36c9bf59f2554e41e335703b4c53c59896b8d
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2022-06-06 20:10:01 +0100

    Update navSlice.js

--- slices/navSlice.js
+++ slices/navSlice.js
@@ -27,3 +27,5 @@ export const { setOrigin, setDestination, setTravelTimeInformation } = navSlice.
 export const selectOrigin = (state) => state.nav.origin;
 export const selectDestination = (state) => state.nav.destination;
 export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
