19276df0736e7e6e53bb2e181499fff849af6925
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2022-06-06 21:55:36 +0100

    changes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import React from 'react';
 import tw from 'tailwind-react-native-classnames';
+import logo from '../assets/whiteicon.png';
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Image source={require('../assets/white icon.png')}/>
+    <SafeAreaView style={styles.container}>
+          <Image source={logo} style={styles.img} />
     </SafeAreaView>
   )
 }
@@ -13,5 +14,16 @@ export default HomeScreen
 const styles = StyleSheet.create({
     text:{
         color: "blue"
-    }
+    },
+    img: {
+        width: "auto",
+        height: "auto",
+        flex: 0.5
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#222',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }
 })
